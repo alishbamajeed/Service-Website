32a2de2d2e2c92124ab0f36d64fb75debc6fc7ac
alishbamajeed <alishbamajeed22@gmail.com>
2025-05-12 00:21:32 +0500

    /

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -5,7 +5,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 1,
     title: "Toy Ecommerce Website",
     description: "A playful and responsive toy store website offering smooth browsing, secure checkout, and vibrant product displays for kids and parents.",
-    image: "/public/toy.png",
+    image: "/public/image/toy.png",
     tags: [ "Next.js", "TailwindCSS" , "TypeScript" , "Sanity" , "Firebase"],
     link: "httpsWeb Application://e-commerce-mauve-rho.vercel.app/"
   },
@@ -13,7 +13,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 2,
     title: "ShopCo Cloth Ecommerce Website",
     description: "A modern and elegant clothing ecommerce website offering a seamless shopping experience with trendy collections, secure checkout, and mobile-friendly design",
-    image: "public/shopco.png",
+    image: "/public/image/shopco.png",
     tags: ["Next.js", "TailwindCSS" , "TypeScript" , "Sanity"],
     link: "https://shop-figma-template.vercel.app/"
   },
@@ -21,7 +21,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 3,
     title: "Car Rental E-Commerce Website",
     description: "A sleek and user-friendly car rental platform that allows customers to browse, book, and pay for vehicles online with ease. Features include real-time availability, secure checkout, and responsive design.",
-    image: "public/car.png",
+    image: "/public/image/car.png",
     tags: [ "Next.js", "TailwindCSS" , "TypeScript" , "Sanity"],
     link: "https://q2-hakhathon-car-rent-shop-9fr1.vercel.app/"
   },
@@ -29,7 +29,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 4,
     title: "Future Fashion Ecommerce Website",
     description: "A next-gen e-commerce site with smart features, modern design, and a seamless shopping experience",
-    image: "/public/fashion.png",
+    image: "/public/image/fashion.png",
     tags: ["Next.js", "figma", "TailwindCSS", "Firebase"],
     link: "https://e-commerce-i59i.vercel.app/"
   },
@@ -37,7 +37,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 5,
     title: "Furniture Website",
     description: "A stylish and responsive furniture website offering modern designs, easy browsing, and secure online shopping for every space",
-    image: "/public/furniture.png",
+    image: "/public/image/furniture.png",
     tags: ["Next.js", "figma", "TailwindCSS", "Sanity"],
     link: "https://figma-ui-hackathon.vercel.app/"
   },
@@ -45,7 +45,7 @@ export const portfolio: PortfolioItemType[] = [
     id: 6,
     title: "Fully Functionally Blog Website",
     description: " A dynamic and responsive blog platform with post creation, editing, comments, and category filtering — built for seamless reading and content management",
-    image: "/public/blog.png",
+    image: "/public/image/blog.png",
     tags: ["Next.js", "figma", "TailwindCSS", "Firebase"],
     link: "https://full-functionality-blog.vercel.app/"
   }
